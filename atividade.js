ec52060107eb862071d6211fd6bdc452f783590f
Ayrton Oliveira <ayrtonjose_2011@hotmail.com>
2024-09-16 22:40:17 +0000

    atualziando código

--- atividade.js
+++ atividade.js
@@ -1,1 +1,57 @@
 
+//1- 
+console.log("1º- Nome e Idade:")
+const nome = "Ayrton Oliveira"
+var idade = 29
+console.log("Olá, meu nome é " + nome + " e tenho " + idade + " anos.\n")
+
+//2-
+console.log("2º- Nome e Cidade:")
+var cidade = "Recife"
+console.log(nome + " é de " + cidade + ".\n")
+
+//3- 
+console.log("3º- Quatro operações:")
+const num1 = 20
+const num2 = 10
+console.log("Número 1 = 20 e Número 2 = 10")
+soma = num1 + num2
+console.log("Adição: " + num1 + " + " + num2 + " = " + soma)
+subtracao = num1 - num2
+console.log("Subtração: " + num1 + " - " + num2 + " = " + subtracao)
+multiplicacao = num1 * num2
+console.log("Multiplicação: " + num1 + " * " + num2 + " = " + multiplicacao)
+divisao = num1 / num2
+console.log("Divisão: " + num1 + " / " + num2 + " = " + divisao + ".\n" )
+
+//4-
+console.log("4º- Área do triângulo:")
+const base = 10
+const altura = 20
+console.log("A base de um triângulo é " + base + "cm" + " e sua altura é " + altura + "cm" + ", calcule a área do triângulo.")
+console.log("A área do triângulo é = " +  [base*altura]/2 + "cm².\n")
+
+//5-
+console.log("5º- Média:")
+const nota1 = 10
+const nota2 = 8
+const nota3 = 7
+media = [nota1+nota2+nota3] / 3
+console.log("Determinado aluno obteve as seguintnes notas: 10, 8 e 7 na Av11, Av2 e Av3 respectivamente. Calcule a média:")
+console.log("O Resultado da média é: " + media + ".\n")
+
+//6-
+console.log("6º- Desconto:")
+var preco = 100
+var desconto = 20/100
+var precoFinal = preco - [preco*desconto]
+console.log("SÓ HOJE!!!! Kg do Tomate com 20% de desconto!! De R$ " + preco + " por apenas R$ " + precoFinal + ".\n")
+
+//7-
+console.log("7º- IR:")
+const alq4 = 27.5/100
+var salario = 7500
+console.log("O salário de Jucrutes no mês de Agosto foi" + )
+
+
+
